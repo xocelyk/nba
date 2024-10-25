af1eca1c3f8f37baf7c740b3b1118670cdc6e7f9
xocelyk <kylecox2000@gmail.com>
2024-10-25 12:29:43 -0500

    manual data update

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ def simulate_season(training_data: pd.DataFrame, models: Tuple, mean_pace: float
         std_pace,
         year=year,
         num_sims=num_sims,
-        parallel=False
+        parallel=True
     )
     date_string = datetime.datetime.today().strftime('%Y-%m-%d')
     sim_report.to_csv('data/sim_results/sim_report.csv')
